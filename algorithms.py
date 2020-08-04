71919ee7521a6538e274da57fab3ade41293860e
abazarova <45312663+abazarova@users.noreply.github.com>
2020-08-04 19:24:10 +0300

    Update algorithms.py

--- algorithms.py
+++ algorithms.py
@@ -10,151 +10,131 @@ from scipy.sparse import csr_matrix
 from functions import *
 
 
-def noname_fun_one_to_all(fun, x0, grad, grad_params, method, communication_rounds, n, k, h, lr, delta):
+def noname_fun_one_to_all(fun, x0, grad, grad_params, method, communication_rounds, n, k, frq, lr, delta):
     res = minimize(fun, x0, method='BFGS', tol=1e-6)
     x = np.arange(0, communication_rounds, 1)
     y = []
     time_arr = []
     a = np.array([x0 for i in range(k)])
-    fun0 = fun(x0)
     t0 = time.time()
-    for i in range(communication_rounds * n * h):
+    for i in range(communication_rounds * frq):
         e = generate_vector(method, n)
         for j in range(k):
             a[j] -= grad(e, delta, a[j], grad_params[j]) * lr
-        if i % (n * h) == 0:
+        if i % (frq) == 0:
             avg = 0
             for j in range(k):
                 avg += a[j]
             avg /= k
             for j in range(k):
                 a[j] = avg
+            if i == 0:
+                fun0 = fun(a[0])
             y.append((fun(a[0]) - res.fun)/(fun0 - res.fun))
             time_arr.append(time.time() - t0)
     return x, y, time_arr
     
     
-def noname_fun_one_to_one(fun, x0, grad, grad_params, method, communication_rounds, n, k, h, lr, delta):
+def noname_fun_one_to_one(fun, x0, grad, grad_params, method, communication_rounds, n, k, frq, lr, delta):
     res = minimize(fun, x0, method='BFGS', tol=1e-6)
     x = np.arange(0, communication_rounds, 1)
     y = []
     time_arr = []
     a = np.array([x0 for i in range(k)])
-    fun0 = fun(x0)
     t0 = time.time()
-    for i in range(communication_rounds * n * h):
+    for i in range(communication_rounds * frq):
         for j in range(k):
             e = generate_vector(method, n)
             a[j] -= grad(e, delta, a[j], grad_params[j]) * lr
-        if i % (n * h) == 0:
+        if i % (frq) == 0:
             avg = 0
             for j in range(k):
                 avg += a[j]
             avg /= k
             for j in range(k):
                 a[j] = avg
+            if i == 0:
+                fun0 = fun(a[0])
             y.append((fun(a[0]) - res.fun)/(fun0 - res.fun))
             time_arr.append(time.time() - t0)
     return x, y, time_arr
-'''
 
-def logreg_func_one_to_all(generation_mode, communication_rounds, X, y, k, h):
-    n = X.shape[1]
-    L = 0.25 * np.max(np.sum(np.square(X), axis=1))
-    arr_X = np.array_split(X, k)
-    arr_y = np.array_split(y, k) 
-    delta = 0.001
-    lr = 1/L
-    
-    def sigma(w):
-        sigm = 0
-        for i in range(k):
-            sigm += logreg_function(w, arr_X[i], arr_y[i])
-        return sigm
-    
-    w0 = np.random.rand(n)
-    res = minimize(sigma, w0, method='BFGS', tol=1e-6)
+def zo_local_sgd_one_to_all(fun, x0, grad, params, method, n_batches, communication_rounds, n, k, frq, lr, delta):
+    res = minimize(fun, x0, method='BFGS', tol=1e-6)
+    arr_X = []
+    arr_y = []
+    for j in range(k):
+        arr_X.append(params[j][0])
+        arr_y.append(params[j][1])
+    arr_X_split = [np.array_split(arr_X[i], n_batches) for i in range(k)]
+    arr_y_split = [np.array_split(arr_y[i], n_batches) for i in range(k)]
     x = np.arange(0, communication_rounds, 1)
     y = []
     time_arr = []
-    w = np.zeros((k, n))
-    sigma0 = sigma(np.zeros(n))
+    a = np.array([x0 for i in range(k)])
     t0 = time.time()
-    for i in range(communication_rounds * n * h):
-        e = generate_vector(generation_mode, n)
+    for i in range(communication_rounds * frq):
+        e = generate_vector(method, n)
         for j in range(k):
-            w[j] -= pseudo_grad_logreg(w[j], arr_X[j], arr_y[j], e, delta) * lr
-        if i % (n * h) == 0:
+            q = random.randint(0, n_batches - 1)
+            a[j] -= grad(e, delta, a[j], [arr_X_split[j][q], arr_y_split[j][q]]) * lr
+        if i % (frq) == 0:
             avg = 0
             for j in range(k):
-                avg += w[j]
+                avg += a[j]
             avg /= k
             for j in range(k):
-                w[j] = avg
-            y.append((sigma(avg) - res.fun)/(sigma0 - res.fun))
+                a[j] = avg
+            if i == 0:
+                fun0 = fun(a[0])
+            y.append((fun(a[0]) - res.fun)/(fun0 - res.fun))
             time_arr.append(time.time() - t0)
     return x, y, time_arr
 
-def logreg_func_one_to_one(generation_mode, communication_rounds, X, y, k, h):
-    n = X.shape[1]
-    L = 0.25 * np.max(np.sum(np.square(X), axis=1))
-    arr_X = np.array_split(X, k)
-    arr_y = np.array_split(y, k) 
-    delta = 0.001
-    lr = 1/L
-    
-    def sigma(w):
-        sigm = 0
-        for i in range(k):
-            sigm += logreg_function(w, arr_X[i], arr_y[i])
-        return sigm
-    
-    w0 = np.random.rand(n)
-    res = minimize(sigma, w0, method='BFGS', tol=1e-6)
+def zo_local_sgd_one_to_one(fun, x0, grad, params, method, n_batches, communication_rounds, n, k, frq, lr, delta):
+    res = minimize(fun, x0, method='BFGS', tol=1e-6)
+    arr_X = []
+    arr_y = []
+    for j in range(k):
+        arr_X.append(params[j][0])
+        arr_y.append(params[j][1])
+    arr_X_split = [np.array_split(arr_X[i], n_batches) for i in range(k)]
+    arr_y_split = [np.array_split(arr_y[i], n_batches) for i in range(k)]
     x = np.arange(0, communication_rounds, 1)
     y = []
     time_arr = []
-    w = np.zeros((k, n))
-    sigma0 = sigma(np.zeros(n))
+    a = np.array([x0 for i in range(k)])
     t0 = time.time()
-    for i in range(communication_rounds * n * h):
+    for i in range(communication_rounds * frq):
         for j in range(k):
-            e = generate_vector(generation_mode, n)
-            w[j] -= pseudo_grad_logreg(w[j], arr_X[j], arr_y[j], e, delta) * lr
-        if i % (n * h) == 0:
+            e = generate_vector(method, n)
+            q = random.randint(0, n_batches - 1)
+            a[j] -= grad(e, delta, a[j], [arr_X_split[j][q], arr_y_split[j][q]]) * lr
+        if i % (frq) == 0:
             avg = 0
             for j in range(k):
-                avg += w[j]
+                avg += a[j]
             avg /= k
             for j in range(k):
-                w[j] = avg
-            y.append((sigma(avg) - res.fun)/(sigma0 - res.fun))
+                a[j] = avg
+            if i == 0:
+                fun0 = fun(a[0])
+            y.append((fun(a[0]) - res.fun)/(fun0 - res.fun))
             time_arr.append(time.time() - t0)
     return x, y, time_arr
 
-def gradient_descent(communication_rounds, X, y, k, h):
-    n = X.shape[1]
-    L = 0.25 * np.max(np.sum(np.square(X), axis=1))
-    arr_X = np.array_split(X, k)
-    arr_y = np.array_split(y, k) 
+def gradient_descent(fun, w0, grad_params, communication_rounds, n, k, h, L):
     delta = 0.001
     lr = 1/L
-    
-    def sigma(w):
-        sigm = 0
-        for i in range(k):
-            sigm += logreg_function(w, arr_X[i], arr_y[i])
-        return sigm
-    
+    res = minimize(fun, w0, method='BFGS', tol=1e-6)
     x = np.arange(0, communication_rounds, 1)
     y = []
     time_arr = []
-    w = np.zeros((k, n))
-    sigma0 = sigma(np.zeros(n))
+    w = np.array([w0 for i in range(k)])
     t0 = time.time()
     for i in range(communication_rounds * h):
         for j in range(k):
-            w[j] -= logreg_grad(w[j], arr_X[j], arr_y[j]) * lr
+            w[j] -= logreg_grad(w[j], grad_params[j]) * lr
         if i % h == 0:
             avg = 0
             for j in range(k):
@@ -162,7 +142,41 @@ def gradient_descent(communication_rounds, X, y, k, h):
             avg /= k
             for j in range(k):
                 w[j] = avg
-            y.append((sigma(avg) - res.fun)/(sigma0 - res.fun))
+            if i == 0:
+                fun0 = fun(w[0])
+            y.append((fun(avg) - res.fun)/(fun0 - res.fun))
             time_arr.append(time.time() - t0)
     return x, y, time_arr
-'''
+
+def s_gradient_descent(fun, w0, grad_params, n_batches, communication_rounds, n, k, h, L):
+    delta = 0.001
+    lr = 1/L
+    res = minimize(fun, w0, method='BFGS', tol=1e-6)
+    arr_X = []
+    arr_y = []
+    for j in range(k):
+        arr_X.append(grad_params[j][0])
+        arr_y.append(grad_params[j][1])
+    arr_X_split = [np.array_split(arr_X[i], n_batches) for i in range(k)]
+    arr_y_split = [np.array_split(arr_y[i], n_batches) for i in range(k)]
+    x = np.arange(0, communication_rounds, 1)
+    y = []
+    time_arr = []
+    w = np.array([w0 for i in range(k)])
+    t0 = time.time()
+    for i in range(communication_rounds * h):
+        for j in range(k):
+            q = random.randint(0, n_batches - 1)
+            w[j] -= logreg_grad(w[j], [arr_X_split[j][q], arr_y_split[j][q]]) * lr
+        if i % h == 0:
+            avg = 0
+            for j in range(k):
+                avg += w[j]
+            avg /= k
+            for j in range(k):
+                w[j] = avg
+            if i == 0:
+                fun0 = fun(w[0])
+            y.append((fun(avg) - res.fun)/(fun0 - res.fun))
+            time_arr.append(time.time() - t0)
+    return x, y, time_arr
